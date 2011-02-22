f6efd9851e67dbfef9b1e30b7bc274b3bb4aaf83
daltonrooney <daltonrooney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-22 22:28:59 +0000

    0.4.0 adds "Replace all MP3s" option, default audio folder location, and drop-in compatibility with WP Audio Player Format
    git-svn-id: https://plugins.svn.wordpress.org/haiku-minimalist-audio-player/trunk@349478 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- haiku-player.php
+++ haiku-player.php
@@ -4,37 +4,66 @@ Plugin Name: Haiku - minimalist audio player
 Plugin URI: http://madebyraygun.com/lab/haiku
 Description: A simple HTML5-based audio player that inserts a text link or graphical player for audio playback.
 Author: Dalton Rooney
-Version: 0.3.1
+Version: 0.4.0
 Author URI: http://madebyraygun.com
 */ 
 
-$haiku_player_version = "0.3.1";
+$haiku_player_version = "0.4.0";
+
 // add our default options if they're not already there:
 if (get_option('haiku_player_version')  != $haiku_player_version) {
     update_option('haiku_player_version', $haiku_player_version);}
 add_option("haiku_player_show_support", 'true'); 
 add_option("haiku_player_show_graphical", 'false'); 
 add_option("haiku_player_analytics", 'false'); 
+add_option("haiku_player_default_location", ''); 
+add_option("haiku_player_replace_audio_player", ''); 
+add_option("haiku_player_replace_mp3_links", ''); 
    
 // now let's grab the options table data
 $haiku_player_version = get_option('haiku_player_version');
 $haiku_player_show_support = get_option('haiku_player_show_support');
 $haiku_player_show_graphical = get_option('haiku_player_show_graphical');
 $haiku_player_analytics = get_option('haiku_player_analytics');
+$haiku_player_default_location = get_option('haiku_player_default_location');
+$haiku_player_replace_audio_player = get_option('haiku_player_replace_audio_player');
+$haiku_player_replace_mp3_links = get_option('haiku_player_replace_mp3_links');
 
 //set up defaults if these fields are empty
 if (empty($haiku_player_show_graphical)) {$haiku_player_show_graphical = "false";}
 if (empty($haiku_player_analytics)) {$haiku_player_analytics = "false";}
+if (empty($haiku_player_replace_audio_player)) {$haiku_player_replace_audio_player = "false";}
+
+function replace_audio($content) { //finds the old audio player shortcode and rewrites it
+  $content = preg_replace('/\[audio:/','[haiku url=',$content,1);
+  return $content;
+}
+
+if (!empty($haiku_player_replace_audio_player)) { //only run the audio tag replacement filter if the user selected it
+	add_filter('the_content', replace_audio);
+}
+
+function replace_mp3_links($content) {
+  $pattern = "/<a ([^=]+=['\"][^\"']+['\"] )*href=['\"](([^\"']+\.mp3))['\"]( [^=]+=['\"][^\"']+['\"])*>([^<]+)<\/a>/i"; //props to WordPress Audio Player for the regex
+  $replacement = '[haiku url=$2 defaultpath=disabled]';
+  $content = preg_replace($pattern, $replacement, $content,1);
+  return $content;
+}
+
+if (!empty($haiku_player_replace_mp3_links)) { //only run the MP3 link replacement filter if the user selected it
+	add_filter('the_content', replace_mp3_links);
+}
 
 add_shortcode('haiku', 'haiku_player_shortcode');
 // define the shortcode function
 
 function haiku_player_shortcode($atts) {
-	global $haiku_player_show_graphical, $haiku_player_analytics;
+	global $haiku_player_show_graphical, $haiku_player_default_location, $haiku_player_analytics;
 	STATIC $i = 1;
 	extract(shortcode_atts(array(
 		'url'	=> '',
 		'title'	=> '',
+		'defaultpath' => '',
 		'graphical' => $haiku_player_show_graphical
 	), $atts));
 	// stuff that loads when the shortcode is called goes here
@@ -46,7 +75,15 @@ function haiku_player_shortcode($atts) {
 			<ul id="player-buttons'.$i.'" class="player-buttons"> 
 				<li class="play"';
 				if ($haiku_player_analytics == "true") { $haiku_player_shortcode .=  ' onClick="_gaq.push([\'_trackEvent\', \'Audio\', \'Play\', \''.$title.'\']);"';}
-				$haiku_player_shortcode .= '><a title="Listen to '.$title.'" class="play" href="'.$url.'">play</a></li> 
+				$haiku_player_shortcode .= '><a title="Listen to '.$title.'" class="play" href="';
+				
+				if (!empty($haiku_player_default_location) && $defaultpath !="disabled") {
+					$haiku_player_shortcode .= site_url() . $haiku_player_default_location . "/";
+				}
+				
+				$haiku_player_shortcode .= $url;
+				
+				$haiku_player_shortcode .= '">play</a></li> 
 				<li class="stop"><a href="javascript: void(0);">stop</a></li>';
 				
 				if(!empty($title)) { $haiku_player_shortcode .= '<li class="title">'.esc_attr($title).'</li>'; }
@@ -58,7 +95,15 @@ function haiku_player_shortcode($atts) {
 	
 	<div id="haiku-player'.$i.'" class="haiku-player"></div>
 	
-		<div id="player-container'.$i.'" class="player-container"><div id="haiku-button'.$i.'" class="haiku-button"><a title="Listen to '.$title.'" class="play" href="'.$url.'"';
+		<div id="player-container'.$i.'" class="player-container"><div id="haiku-button'.$i.'" class="haiku-button"><a title="Listen to '.$title.'" class="play" href="';
+				
+				if (!empty($haiku_player_default_location) && $defaultpath !="disabled") {
+					$haiku_player_shortcode .= site_url() . $haiku_player_default_location . "/";
+				}
+				
+				$haiku_player_shortcode .= $url;
+				
+				$haiku_player_shortcode .= '"';
 		if ($haiku_player_analytics == "true") 
 			{$haiku_player_shortcode .=  ' onClick="_gaq.push([\'_trackEvent\', \'Audio\', \'Play\', \''.$title.'\']);"';}
 		$haiku_player_shortcode .= '><img alt="Listen to '.$title.'" class="listen" src="';
@@ -79,7 +124,7 @@ if( !is_admin()){
    wp_enqueue_script('jquery');
    wp_register_script('jplayer', plugins_url( '/js/jquery.jplayer.min.js', __FILE__ ), false, '1.2', true); 
    wp_enqueue_script('jplayer');
-   wp_register_script('haiku-player', plugins_url( '/js/haiku-player.js', __FILE__ ), false, '0.2.0', true); 
+   wp_register_script('haiku-player', plugins_url( '/js/haiku-player.js', __FILE__ ), false, $haiku_player_version, true); 
    wp_enqueue_script('haiku-player');
  wp_register_script('jquery-ui-custom', plugins_url( '/js/jquery-ui-custom.min.js', __FILE__ ), false, '1.8.7', true); 
    wp_enqueue_script('jquery-ui-custom');
@@ -87,9 +132,10 @@ if( !is_admin()){
 
 
 function haiku_player_head() {
+global $haiku_player_version;
 	echo '
 <!-- loaded by Haiku audio player plugin-->
-<link rel="stylesheet" type="text/css" href="' .  plugins_url( 'haiku-player.css', __FILE__ ) . '?ver=0.2.0" />
+<link rel="stylesheet" type="text/css" href="' .  plugins_url( 'haiku-player.css', __FILE__ ) . '?ver='.$haiku_player_version.'" />
 <script type="text/javascript">
 var jplayerswf = "'. plugins_url( '/js/', __FILE__ ) . '";
 </script>
@@ -107,12 +153,11 @@ function add_haiku_player_option_page() {
 	add_options_page('Haiku Player', 'Haiku Player', 6, __FILE__, 'haiku_player_options_page');
 }
 function haiku_player_options_page() { 	// Output the options page
-	global $haiku_player_version, $haiku_player_show_support, $haiku_player_show_graphical; ?>
-	<div class="wrap" style="width:500px">
+	global $haiku_player_version, $haiku_player_show_support, $haiku_player_default_location,  $haiku_player_show_graphical, $haiku_player_replace_audio_player, $haiku_player_replace_mp3_links; ?>
+	<div class="wrap" style="width:800px">
 	
 
 <form method="post" action="options.php">
-<?php global $haiku_player_show_graphical, $haiku_player_analytics;?>
 
 <?php wp_nonce_field('update-options'); ?>
 
@@ -121,6 +166,12 @@ function haiku_player_options_page() { 	// Output the options page
 <h2>Haiku Player Settings</h2>
 
 <table class="form-table">
+
+<tr valign="top">
+	<th scope="row">Default file location (optional)</th><br />
+	<td><?php echo site_url();?><input type="text" name="haiku_player_default_location" value="<?php if (!empty($haiku_player_default_location)) {echo $haiku_player_default_location; }?>"/>
+	</td>
+</tr>
 
 <tr valign="top">
 	<th scope="row">Enable Google Analytics</th>
@@ -133,9 +184,21 @@ function haiku_player_options_page() { 	// Output the options page
 	<td><input type="checkbox" name="haiku_player_show_graphical" value="true" <?php if ($haiku_player_show_graphical=="true") {echo' checked="checked"'; }?>/>
 	</td>
 </tr>
+
+<tr valign="top">
+	<th scope="row">Replace all mp3 links</th>
+	<td><input type="checkbox" name="haiku_player_replace_mp3_links" value="true" <?php if ($haiku_player_replace_mp3_links=="true") {echo' checked="checked"'; }?>/>
+	</td>
+</tr>
+
+<tr valign="top">
+	<th scope="row">Replace WP Audio Player [audio: file.mp3] syntax</th>
+	<td><input type="checkbox" name="haiku_player_replace_audio_player" value="true" <?php if ($haiku_player_replace_audio_player=="true") {echo' checked="checked"'; }?>/>
+	</td>
+</tr>
 </table>
 
-<input type="hidden" name="page_options" value="haiku_player_show_graphical, haiku_player_analytics" />
+<input type="hidden" name="page_options" value="haiku_player_show_graphical, haiku_player_analytics, haiku_player_default_location,haiku_player_replace_audio_player, haiku_player_replace_mp3_links" />
 <input type="hidden" name="action" value="update" />	
 <p class="submit">
 <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
@@ -173,11 +236,17 @@ function haiku_player_options_page() { 	// Output the options page
 
 
 		<h2>Reference</h2>
-		<p>Use the shortcode <code>[haiku url="http://example.com/file.mp3" title="Title of audio file"]</code> to play an audio file. Be sure to use the full URL to the audio file. The title field is recommended for search engine and accessibility purposes and required if you are using Google Analytics.</p>
+		<p>Use the shortcode <code>[haiku url="http://example.com/file.mp3" title="Title of audio file"]</code> to play an audio file. Use the full URL of the audio file unless you've set a default file location. The title field is recommended for search engine and accessibility purposes and required if you are using Google Analytics.</p>
+		
+		<p>The default file location field allows you to specify a folder in your site for your MP3 files. If, for example, all of your audio files are in http://yoursite.com/audio, set the default folder to /audio, and use the shortcode like this: [haiku url="file.mp3"]. This is also helpful if you're replacing an existing WordPress Audio Player installation. You can overwrite this setting on a per-player basis with the attribute defaultpath=disabled. For example, if you wanted to link to a file in a different folder you would use the shortcode [haiku url="http://yoursite/audiofolder2/file.mp3" defaultpath=disabled].</p>
 		
 		<p>The Google Analytics setting enables a script which tracks play events in your Google Analytics account using the title field. You must already have Google Analytics tracking installed on your site, using the asynchronous version of the script in the head of your HTML document.</p>
 		
-		<p>The player now supports a graphical mode, which is turned off by default. Enable graphical mode by changing the default setting in the settings panel or by adding the attribute <code>graphical="true"</code> to your shortcode. Can be overridden on a per-player basis.</p>
+		<p>The player includes the ability to automatically turn all MP3 links into an audio player instance. Simply check the "Replace all mp3 links" box. You may experience problems if you have other plugins that also override MP3 links, like Shadowbox.</p>
+		
+		<p>The player is now drop-in compatible with WordPress Audio Player. If you're replacing a WordPress Audio Player install, check the "replace WP Audio Player" box to automatically replace all instances of the WP Audio Player shortcode. (WP Audio Player must be disabled or removed for this to work). The Haiku shortcode is still the recommended format as it allows the "Title" field for Google Analytics support.</p>
+				
+		<p>The player includes a graphical mode, which is turned off by default. Enable graphical mode by changing the default setting in the settings panel or by adding the attribute <code>graphical="true"</code> to your shortcode. Can be overridden on a per-player basis.</p>
 		
 		<p>The graphical player looks like this:</p>
 		<img src ="<?php echo plugins_url( 'resources/player-example.png', __FILE__ )?>" alt="player example" height="50" width="178"/>
--- readme.txt
+++ readme.txt
@@ -4,21 +4,29 @@ Tags: audio player, jplayer, html5, audio, flash, mp3, music, minimalist, jquery
 Requires at least: 2.8
 Donate link: http://madebyraygun.com/donate/
 Tested up to: 3.1
-Stable tag: 0.3.1
+Stable tag: 0.4.0
 
 == Description ==
 
-A simple HTML5-based audio player that inserts a text link or graphical player for audio playback. Fully accessible and degrades gracefully, and includes Google Analytics tracking of audio plays. Based on [jPlayer](http://www.jplayer.org/).
+A simple HTML5-based audio player that inserts a text link or graphical player for audio playback. Compatible with WP Audio Player style links, fully accessible and degrades gracefully, and includes Google Analytics tracking of audio plays. Based on [jPlayer](http://www.jplayer.org/).
 
 == Installation ==
 
-Extract the zip file and upload the contents to the wp-content/plugins/ directory of your WordPress installation and then activate the plugin from plugins page. 
+Extract the zip file and upload the contents to the wp-content/plugins/ directory of your WordPress installation, and then activate the plugin from plugins page. 
 
-Use shortcode [haiku url="http://example.com/file.mp3" title="Title of audio file"] to play an audio file. Be sure to use the full URL to the audio file. The title field is recommended for search engine and accessibility purposes and required if you are using Google Analytics.
+Use the shortcode [haiku url="http://example.com/file.mp3" title="Title of audio file"] to play an audio file. Use the full URL of the audio file unless you've set a default file location in the settings page. The title field is recommended for search engine and accessibility purposes and required if you are using Google Analytics.
 
+= Settings Options =
+
+The default file location field allows you to specify a folder in your site for your MP3 files. If, for example, all of your audio files are in http://yoursite.com/audio, set the default folder to /audio, and use the shortcode like this: [haiku url="file.mp3"]. This is also helpful if you're replacing an existing WordPress Audio Player installation. You can overwrite this setting on a per-player basis with the attribute defaultpath=disabled. For example, if you wanted to link to a file in a different folder you would use the shortcode [haiku url="http://yoursite/audiofolder2/file.mp3" defaultpath=disabled].
+		
 The Google Analytics setting enables a script which tracks play events in your Google Analytics account using the title field. You must already have Google Analytics tracking installed on your site, using the asynchronous version of the script in the head of your HTML document.
-
-The player now supports a graphical mode, which is turned off by default. Enable graphical mode by changing the default setting in the settings panel or by adding the attribute graphical="true" to your shortcode. Can be overridden on a per-player basis.
+		
+The player includes the ability to automatically turn all MP3 links into an audio player instance. Simply check the "Replace all mp3 links" checkbox on the settings. You may experience conflicts if you have other plugins that also override MP3 links, like Shadowbox.
+		
+The player is now drop-in compatible with WordPress Audio Player. If you're replacing a WordPress Audio Player install, check the "replace WP Audio Player" box to automatically replace all instances of the WP Audio Player shortcode. (WP Audio Player must be disabled or removed for this to work). The Haiku shortcode is still the recommended format as it allows the "Title" field for Google Analytics support.
+				
+The player includes a graphical mode, which is turned off by default. Enable graphical mode by changing the default setting in the settings panel or by adding the attribute graphical="true" to your shortcode. Can be overridden on a per-player basis.
 
 Please note that the graphical player is at an early stage of development and should be tested before you deploy it to a large audience. It is likely that the design of the player will change in future versions. It's just HTML & CSS, so feel free to experiment with your own examples.
 
@@ -37,7 +45,7 @@ Make sure you include the title attribute for the shortcode, that's what Analyti
 
 = The graphical mode is all screwed up! =
 
-The graphical mode is just basic HTML & CSS. I've done my best to make it compatible with a variety of themes, but I can't test every single one. Send me a link via the [forum](http://madebyraygun.com/forum) and I'll try to help you out.
+The graphical mode is just basic HTML & CSS. I've done my best to make it compatible with a variety of themes, but I can't test every single one. Send me a link via the [forum](http://madebyraygun.com/support/forum) and I'll try to help you out.
 
 = How can I customize the graphical mode with my own colors, text, or what have you? =
 
@@ -57,6 +65,14 @@ You might want to drop a link in the [forum](http://madebyraygun.com/forum).
 == Changelog ==
 
 
+
+= 0.4.0 =
+
+* Option to replace all mp3 links with a player instance.
+
+* Drop-in compatibility with the WordPress Audio Player plugin shortcode.
+
+* Option for default folder for all of your audio files.
 
 = 0.3.1 =
 
