64e145d9e8f68427c13320520b424f8a0a6c41a2
momnt <momnt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 17:19:42 +0000

    adding 1.0.0 tag
    git-svn-id: https://plugins.svn.wordpress.org/haiku-minimalist-audio-player/tags/1.0.0@674172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/haiku-player.js
+++ js/haiku-player.js
@@ -1,152 +1,210 @@
-jQuery(document).ready(function($) {
- 
- $(window).load(function() {	
-	$('.player-container').css('visibility', 'visible').show();
- 
+/**
+ *
+ *  Main Haiku js functions
+ *
+ */ 
+;(function($){
 
-	 /*Text Version*/
-	 
-	  	$("div[id^=haiku-text-player]").each(function() {
-			var num = this.id.match(/haiku-text-player(\d+)/)[1];
-			var ctrlId = "div#text-player-container" + num + " ul#player-buttons" + num;
-			var audioFile = $(ctrlId + " li.play a").attr("href");
-			 $(this).jPlayer({
-				ready: function () {
-					this.element.jPlayer("setFile", audioFile);
-				},
-				customCssIds: true,
-				swfPath: jplayerswf,
-				errorAlerts:true,
-				nativeSuport:false
-			})
-			$(ctrlId + " li.play").click(function() {
-				stopAllPlayers();
-				$("div#haiku-text-player" + num).jPlayer("play");
-				showStopBtn();
-				return false;
-			});
-			$(ctrlId + " li.stop").click(function() {
-				$("div#haiku-text-player" + num).jPlayer("stop");
-				showPlayBtn();
-				return false;
-			});
-			function stopAllPlayers() {
-				$("div.haiku-text-player").jPlayer("stop");
-				$("div.text-player-container ul.player-buttons li.stop").hide();					$("div.text-player-container ul.player-buttons li.play").show();
-			}
-			function showStopBtn() {
-				$(ctrlId + " li.play").fadeOut(100, function(){
-					$(ctrlId + " li.stop").css("display","inline").fadeIn(100);});
-			}
-			function showPlayBtn() {
-				$(ctrlId + " li.stop").fadeOut(100, function(){
-					$(ctrlId + " li.play").fadeIn(10);});
-			}
 
-		});
 
-	/*Graphical version*/
+    /**
+     *  Text players
+     */
+    $.fn.textPlayers = function() {
 
-		$("div[id^=haiku-player]").each(function() {
-		
-			var num = this.id.match(/haiku-player(\d+)/)[1];
-			var buttonId = "div#haiku-button" + num;
-			var jpPlayTime = $("#jplayer_play_time" + num);
-			var jpTotalTime = $("#jplayer_total_time" + num);
-			var audioFile = $(buttonId + " a").attr("href");
-			currentPlayer = 0;
-			
-			$(this).jPlayer({
-				ready: function () {
-					this.element.jPlayer("setFile", audioFile);
-				},
-				customCssIds: true,
-				swfPath: jplayerswf,
-				errorAlerts:true,
-				nativeSuport:false
-			})
-			
-			.jPlayer("onSoundComplete", function() {
-			stopPreviousPlayer();
-			})
-			
-			.jPlayer("onProgressChange", function(lp,ppr,ppa,pt,tt) {
-	 		var lpInt = parseInt(lp);
-	 		var ppaInt = parseInt(ppa);
-	 		global_lp = lpInt;
-	 		jpPlayTime.text($.jPlayer.convertTime(pt) + " / ");
-			jpTotalTime.text($.jPlayer.convertTime(tt));
+        var player = $('div.haiku-text-player');
 
-	 		$('#sliderPlayback' + num).slider('option', 'value', ppaInt);
-			});
-	 
-			// Slider
-			$('#sliderPlayback' + num).slider({
-				max: 100,
-				range: 'min',
-				animate: true,
-				slide: function(event, ui) {
-					$('#haiku-player' + num).jPlayer("playHead", ui.value*(100.0/global_lp));
-					$(buttonId + " li.play").hide();
-					$(buttonId + " li.pause").fadeIn(10);
-				}
-			});
-		
-			$(buttonId + " a.play").click(function() {
-				stopPreviousPlayer();
-				$(buttonId + " a img.listen").hide();
-				$(buttonId + " li.play").hide();
-				$("ul#controls" + num).fadeIn('fast');
-				$("ul#info" + num).fadeIn('fast');
-				$("div#haiku-player" + num).jPlayer("play");
-				currentPlayer = num;
-				return false;
-			});	
-		
-			$(buttonId + " li.pause").click(function() {
-				$("div#haiku-player" + num).jPlayer("pause");
-				showPlayBtn();
-				return false;
-			});
-			
-			$(buttonId + " li.play").click(function() {
-				$("div#haiku-player" + num).jPlayer("play");
-				showPauseBtn();
-				return false;
-			});
-			
-			$(buttonId + " li.stop").click(function() {
-				$("div#haiku-player" + num).jPlayer("stop");
-				showDefault();
-				return false;
-			});
-			
-			function stopPreviousPlayer() {
-				$("div#haiku-player" + currentPlayer).jPlayer("stop");
-				showPauseBtn();
-				$("ul#controls" + currentPlayer).hide();	
-				$("ul#info" + currentPlayer).hide();	
-				$("div#haiku-button" + currentPlayer + " a img.listen").fadeIn('fast');
-			}
-			
-			function showPlayBtn() {
-			$(buttonId + " li.pause").hide();
-			$(buttonId + " li.play").fadeIn(10);
-			}
-			
-			function showPauseBtn() {
-			$(buttonId + " li.play").hide();
-			$(buttonId + " li.pause").fadeIn(10);
-			}
-				
-			function showDefault() {
-				$("ul#controls" + num).hide();
-				$("ul#info" + num).hide();
-				showPauseBtn();
-				$(buttonId + " a img.listen").fadeIn(10);
-			}
-			
-		});
-	});	
-});
- 
+        player.each(function(){
+
+            var $this, id, container, play, pause, stop,
+                loopPlayer, mp3File, ogaFile, selectors;
+
+            $this = $(this);
+            id = this.id.match(/haiku-text-player(\d+)/)[1];
+            container = $this.next('.haiku-container-' + id);
+
+            play  = container.find('#haiku-play-' + id);
+            pause = container.find('#haiku-pause-' + id);
+            stop  = container.find('#haiku-stop-' + id);
+
+            loopPlayer = false;
+
+            if(container.is('.haiku-loop'))
+                loopPlayer = true;
+
+            selectors = {
+                play: '#haiku-play-'  + id,
+                pause: '#haiku-pause-' + id,
+                stop: '#haiku-stop-'  + id,
+                duration: '#haiku-duration-' + id,
+                currentTime: '#haiku-current-time-' + id,
+                noSolution: '.haiku-no-solution'
+            };
+
+            mp3File = play.attr('href');
+
+            if( play.attr('data-haikuoga') ) {
+                
+                ogaFile = play.attr('data-haikuoga');
+
+                $this.jPlayer({
+                    ready: function () {
+                        $(this).jPlayer('setMedia', {
+                            mp3: mp3File,
+                            oga: ogaFile
+                        })
+                    },
+                    swfPath: haiku_jplayerswf_path,
+                    supplied: 'mp3, oga',
+                    solution: 'html, flash',
+                    cssSelectorAncestor: '.haiku-container-' + id,
+                    cssSelector: selectors,
+                    loop: loopPlayer
+                }); // this.jplayer
+
+            } else {
+
+                $this.jPlayer({
+                    ready: function () {
+                        $(this).jPlayer('setMedia', {
+                            mp3: mp3File
+                        })
+                    },
+                    swfPath: haiku_jplayerswf_path,
+                    supplied: 'mp3',
+                    solution: 'html, flash',
+                    cssSelectorAncestor: '.haiku-container-' + id,
+                    cssSelector: selectors,
+                    loop: loopPlayer
+                }); // this.jplayer
+
+            } // endif oga
+
+        }); // player.each
+    }
+
+
+
+    /**
+     *  Graphical players
+     */
+    $.fn.graphPlayers = function() {
+
+        var player = $('div.haiku-graphical-player')
+
+        player.each(function(){
+
+            var $this, id, container, play, pause, stop, 
+                mp3File, ogaFile, selectors, loopPlayer;
+
+            $this = $(this);
+            id = this.id.match(/haiku-graphical-player-(\d+)/)[1];
+            container = $this.next('.haiku-container-' + id);
+            parent = $(this).parent(player);
+
+            play  = container.find('#haiku-play-' + id);
+            pause = container.find('#haiku-pause-' + id);
+            stop  = container.find('#haiku-stop-' + id);
+
+            loopPlayer = false;
+
+
+            if(container.is('.haiku-loop'))
+                loopPlayer = true;
+
+            selectors = {
+                play: '#haiku-play-'  + id,
+                pause: '#haiku-pause-' + id,
+                stop: '#haiku-stop-'  + id,
+                seekBar: '#haiku-seek-bar-' + id,
+                playBar: '#haiku-play-bar-' + id,
+                duration: '#haiku-duration-' + id,
+                currentTime: '#haiku-current-time-' + id,
+                noSolution: '.haiku-no-solution'
+            };
+
+            mp3File = play.attr('href');
+
+            if( play.attr('data-haikuoga') ) {
+                
+                ogaFile = play.attr('data-haikuoga')
+
+                $this.jPlayer({
+                    ready: function () {
+                        $(this).jPlayer('setMedia', {
+                            mp3: mp3File,
+                            oga: ogaFile
+                        })
+                    },
+                    swfPath: haiku_jplayerswf_path,
+                    supplied: 'mp3, oga',
+                    solution: 'html, flash',
+                    cssSelectorAncestor: '.haiku-container-' + id,
+                    cssSelector: selectors,
+                    loop: loopPlayer
+                }); // this.jplayer
+
+            } else {
+
+                $this.jPlayer({
+                    ready: function () {
+                        $(this).jPlayer('setMedia', {
+                            mp3: mp3File
+                        })
+                    },
+                    swfPath: haiku_jplayerswf_path,
+                    supplied: 'mp3',
+                    solution: 'html, flash',
+                    cssSelectorAncestor: '.haiku-container-' + id,
+                    cssSelector: selectors,
+                    loop: loopPlayer
+                }); // this.jplayer
+
+            } // endif oga
+
+
+            // lil layout fix for some browsers
+            pause.hide().css('padding-right','0');
+
+            // play button
+            play.click(function(){
+
+                $(this).hide();
+                pause.fadeIn(100);
+                parent.jPlayer('play');
+
+                return false;
+            });
+
+            // pause button
+            pause.click(function(){
+
+                $(this).hide();
+                play.fadeIn(100).attr('title','Resume');
+                parent.jPlayer('pause');
+
+                return false;
+            });
+
+            // stop button
+            stop.click(function(){
+
+                pause.hide();
+                play.fadeIn(100);
+                parent.jPlayer('stop');
+
+                return false;
+            });
+
+        }); // player.each
+
+    }
+
+    $(window).load(function(){
+
+        $.fn.textPlayers();
+        $.fn.graphPlayers();
+
+    })
+
+})(jQuery);  
